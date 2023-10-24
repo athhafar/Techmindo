d1afd0e616ab22ff4ba34e78eaa08b469ac4b9a0
athhafar_ <athallahfarel6@gmail.com>
2023-10-24 12:20:48 +0700

    fix home

---
+++ src/pages/home/home-page.svelte
@@ -0,0 +1,12 @@
+<script>
+import {Page} from 'framework7-svelte';
+</script>
+
+<Page>
+    <div class="">tes123</div>
+    <div class="">tes lagi</div>
+
+    <div class="card">
+        
+    </div>
+</Page>
